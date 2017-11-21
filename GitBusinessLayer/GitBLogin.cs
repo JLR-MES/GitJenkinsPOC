14f3132c986218e70a86187fe5ba3cefaf5b47d6
JLR-MES <janmejay.thakur@tatatechnologies.com>
2017-11-21 16:29:36 +0530

    Added code for mainForm

--- GitBusinessLayer/GitBLogin.cs
+++ GitBusinessLayer/GitBLogin.cs
@@ -9,13 +9,35 @@ namespace GitBusinessLayer
 {
     public class GitBLogin
     {
-        public DataSet LoginUser(string strLoginID, string strPassword)
+
+        /// <summary>
+        ///This method will call Datalayer menthod  GetUserDetails
+        /// </summary>
+        /// <param name="strLoginID"></param>
+        /// <param name="strPassword"></param>
+        /// <returns></returns>
+        public DataSet GetUserDetails(string strLoginID)
         {
             DataSet dsUser = new DataSet();
             GitDLogin clsGitDLogin = new GitDLogin();
 
-            dsUser = clsGitDLogin.LoginUser(strLoginID, strPassword);
+            dsUser = clsGitDLogin.GetUserDetails(strLoginID);
             return dsUser;
         }
+
+        /// <summary>
+        ///This method will call Datalayer menthod  GetUserDetails
+        /// </summary>
+        /// <param name="strLoginID"></param>
+        /// <param name="strPassword"></param>
+        /// <returns></returns>
+        public int IsValidUser(string strLoginID,string strLoginPassword)
+        {
+            GitDLogin clsGitDLogin = new GitDLogin();
+
+            int result = clsGitDLogin.IsValidUser(strLoginID, strLoginPassword);
+            return result;
+        }
+
     }
-    }
+}
